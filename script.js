937e20b276929b688dddac9ee7af95860d78825d
Marc <07marcmepri@gmail.com>
2025-01-25 15:51:06 +0100

    added new progress bar

--- script.js
+++ script.js
@@ -14,10 +14,21 @@ function updateDOM(){
     let hour = minute * 60;
     let day = hour * 24;
 
-    dayDOM.innerHTML = Math.floor(distance/day);
-    hourDOM.innerHTML = Math.floor(distance%day/hour);
-    minuteDOM.innerHTML = Math.floor(distance%hour/minute);
-    secondDOM.innerHTML = Math.floor(distance%minute/second);
+    dayDOM.innerHTML = Math.floor(distance/day) + "d";
+    hourDOM.innerHTML = Math.floor(distance%day/hour) + "h";
+    minuteDOM.innerHTML = Math.floor(distance%hour/minute) + "m";
+    secondDOM.innerHTML = Math.floor(distance%minute/second) + "s";
 }
 
 setInterval(updateDOM, 1000)
+
+// Barra de carga
+const progress = document.querySelector(".progress")
+const date_past = new Date("5september2024");
+const date_today = new Date();
+
+let result_ms = date_today - date_past;
+let result = Math.round(result_ms * (1.1574*Math.pow(10, -8)));
+
+result = result * 0.2739; //0.2739% == 1d
+progress.style.setProperty("--progress", `${result}%`);
